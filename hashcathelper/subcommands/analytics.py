d35b34ae2c53f6314833fbd6fe87cacbfebf0a93
Adrian Vollmer <adrian.vollmer@syss.de>
2021-08-06 10:13:13 +0200

    Move print functions to new file

--- hashcathelper/subcommands/analytics.py
+++ hashcathelper/subcommands/analytics.py
@@ -60,6 +60,8 @@ args.append(argument(
 def analytics(args):
     '''Output interesting statistics'''
     from ..analytics import create_report
+    from ..asciioutput import pretty_print
+
     report = create_report(
         args.hashes,
         args.accounts_plus_passwords,
@@ -81,62 +83,3 @@ def analytics(args):
             f.write(out)
     else:
         print(out, end='')
-
-
-def pretty_print(report):
-    from ..analytics import labels
-    out = ''
-    simple_values = [
-        [v, labels.get(k, k)] for k, v in report.items()
-        if not (isinstance(v, (list, dict)))
-    ]
-    try:
-        import tabulate
-        out += tabulate.tabulate(simple_values) + "\n"
-    except ImportError:
-        import sys
-        print("Error: package 'tabulate' not installed", file=sys.stderr)
-        for val in simple_values:
-            print("%s\t%s" % (val[0], val[1]))
-    for k, v in report.items():
-        if isinstance(v, dict):
-            out += histogram(v, title=labels.get(k, k))
-            out += "\n"
-    return out
-
-
-def histogram(dct, title='', width=50, indent=4):
-    """Create a text-based horizontal bar chart using Unicode"""
-    if not dct:
-        return "%s: No data" % title
-    maxval = max(dct.values())
-    maxwidth = max([len(str(k)) for k in dct.keys()])
-    blocks = [
-        '',
-        '\u258F',  # 1/8
-        '\u258E',  # 2/8
-        '\u258D',  # 3/8
-        '\u258C',  # 4/8
-        '\u258B',  # 5/8
-        '\u258A',  # 6/8
-        '\u2589',  # 7/8
-        '\u2588',  # 8/8
-    ]
-
-    result = ""
-    if title:
-        result += title + '\n'
-    for k, v in dct.items():
-        if k == '':
-            k = '<EMPTY>'
-        line = ' '*indent
-        line += ' '*(maxwidth - len(str(k))) + str(k) + ' '
-        length = v/maxval * width
-        rounded = int(length)
-        remainder = int(round((length - rounded) * 8))
-        line += blocks[-1]*rounded + blocks[remainder]
-        if isinstance(v, int):
-            line += ' %d' % v
-        result += line + '\n'
-
-    return result
